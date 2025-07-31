f31faee05959c7562751d32957a418aac0a542a3
Rem <fh831.cp9gw@gmail.com>
2025-07-31 15:50:29 +0800

    feature : 提示加上中文 fix: 去掉方法名

--- survey_data.js
+++ survey_data.js
@@ -2,61 +2,61 @@ const SURVEY_DATA = [
   {
     "id": 11,
     "name": "arch",
-    "prompt": "the arch on a road"
+    "prompt": "the arch on a road\n（道路上的拱门。）"
   },
   {
     "id": 12,
     "name": "bus;van",
-    "prompt": "a bus and a van are on the road"
+    "prompt": "a bus and a van are on the road\n（一辆公共汽车和一辆货车在路上。）"
   },
   {
     "id": 14,
     "name": "moon;bridge",
-    "prompt": "the moon shines on the bridge far awsy"
+    "prompt": "the moon shines on the bridge far awsy\n（月光照耀在远处的桥上。）"
   },
   {
     "id": 15,
     "name": "pavilion;bridge",
-    "prompt": "a pavilion standing next to the bridge near a lake"
+    "prompt": "a pavilion standing next to the bridge near a lake\n（一座亭子矗立在靠近湖边的桥旁。）"
   },
   {
     "id": 16,
     "name": "car;tower",
-    "prompt": "a car on the road near a high tower"
+    "prompt": "a car on the road near a high tower\n（一辆汽车在高塔附近的道路上。）"
   },
   {
     "id": 17,
     "name": "person;horse",
-    "prompt": "A person is racing horse on the grassland"
+    "prompt": "A person is racing horse on the grassland\n（一个人在草原上赛马。）"
   },
   {
     "id": 18,
     "name": "Terracotta_Warrior;terracotta_horse",
-    "prompt": "A Terracotta_Warrior with a terracotta_horse by its side"
+    "prompt": "A Terracotta_Warrior with a terracotta_horse by its side\n（一尊兵马俑旁边有一匹陶马。）"
   },
   {
     "id": 24,
     "name": "white_sofa;carpet",
-    "prompt": "a white_sofa with a carpet on the ground"
+    "prompt": "a white_sofa with a carpet on the ground\n（一张白色沙发，地上铺着一块地毯。）"
   },
   {
     "id": 3,
     "name": "laptop;coffee;TV",
-    "prompt": "a laptop and a cup of coffee and a TV in the room"
+    "prompt": "a laptop and a cup of coffee and a TV in the room\n（一台笔记本电脑、一杯咖啡和一台电视在房间里。）"
   },
   {
     "id": 4,
     "name": "cigarettes_box;thermos",
-    "prompt": "a cigarettes_box and a thermos in front of a train's window facing the lake"
+    "prompt": "a cigarettes_box and a thermos in front of a train's window facing the lake\n（一盒香烟和一个保温瓶放在面向湖泊的火车窗前。）"
   },
   {
     "id": 6,
     "name": "pencil_case;laptop;computer_mouse",
-    "prompt": "a pencil_case and a computer_mouse are in front of a laptop"
+    "prompt": "a pencil_case and a computer_mouse are in front of a laptop\n（一个铅笔盒和一个电脑鼠标放在笔记本电脑前面。）"
   },
   {
     "id": 8,
     "name": "statue;building",
-    "prompt": "a statue in the park with a building far away"
+    "prompt": "a statue in the park with a building far away\n（公园里有一座雕像，远处有一栋建筑。）"
   }
 ];
